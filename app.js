c80dc0b9fac48eeef3fec51c3f464edc839c9129
Nebojsa <nebojsacenic97@gmail.com>
2021-10-06 16:52:55 +0200

    Learn GitHub

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-alert('Does this work')
+function multiply(x,y) {
+    return x * y;
+}
+
+function square(x) {
+    return multiply(x,x);
+}
+
+function isRightTriangle(a,b,c){
+    return square(a) + square(b) === square(c);
+}
+
+console.log('before')
+
+isRightTriangle(3,4,5)
+
+console.log('after')
+
+
+const h2 = document.querySelector('h2')
+
+setTimeout(() => {
+    h2.style.color = 'black'
+}, 1000)
